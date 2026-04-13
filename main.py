3a089bd4ae7d2a0fcb1fbc113eaadcb191eeae1d
Raghu <rvaidyan3@gatech.edu>
2026-04-13 18:48:45 -0400

    Add clean inventory UI with Jinja2 templates and Tailwind CSS

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+from db import inventory as inv_db
 from routers.inventory import router as inventory_router
 
 app = FastAPI(title="Meal Planner")
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
 app.include_router(inventory_router)
+
+
+@app.get("/", include_in_schema=False)
+def inventory_ui(request: Request):
+    items = inv_db.list_inventory()
+    return templates.TemplateResponse("inventory.html", {"request": request, "items": items})
